b5618374327d24aa44eda354b68a26cdc64f6505
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-09-01 16:32:58 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,15 +8,22 @@ function Shapes (height, width, background, radius, br_right, br_left, br_top, b
     this.br_top = br_top,
     this.br_bottom = br_bottom
 }
-var square = new Shapes("150px", "150px", "teal", undefined, undefined, undefined, undefined, undefined);
+var square = new Shapes("150px", "150px", "darkblue", undefined, undefined, undefined, undefined, undefined);
 var circle = new Shapes("150px", "150px", "red", "50%", undefined, undefined, undefined, undefined);
-var rectangle = new Shapes("150px", "250px", "red", undefined, undefined, undefined, undefined, undefined);
-var triangle = new Shapes("0px", "0px", "transparent", undefined, "100px solid transparent", "100px solid transparent", undefined, "150px solid red");
+var rectangle = new Shapes("150px", "250px", "orange", undefined, undefined, undefined, undefined, undefined);
+var triangle = new Shapes("0px", "0px", "transparent", undefined, "100px solid transparent", "100px solid transparent", undefined, "150px solid purple");
+var parallelogram = new Shapes("150px", "250px", "darkgreen",undefined, undefined, undefined, undefined, undefined);
 
-var getParentDiv = document.getElementById("container");
-getParentDiv.style.display = "flex";
-getParentDiv.style.flexWrap = "wrap";
-getParentDiv.style.justifyContent = "space-evenly";
+var getMainDiv = document.getElementById("container");
+
+var title = document.createElement("div");
+title.setAttribute("id", "title");
+title.appendChild(document.createTextNode("Shapes with Object"));
+getMainDiv.appendChild(title);
+
+var getParentDiv = document.createElement("div");
+getParentDiv.setAttribute("id","shapes-container")
+getMainDiv.appendChild(getParentDiv);
 
 var sqDiv = document.createElement("div");
 sqDiv.style.height = square.height;
@@ -46,5 +53,11 @@ triDiv.style.borderLeft = triangle.br_left;
 triDiv.style.borderRight = triangle.br_right;
 getParentDiv.appendChild(triDiv);
 
+var paraDiv = document.createElement("div");
+paraDiv.style.height = parallelogram.height;
+paraDiv.style.width = parallelogram.width;
+paraDiv.style.background = parallelogram.background;
+paraDiv.style.transform = "skew(-20deg)"
+getParentDiv.appendChild(paraDiv);
 
 
